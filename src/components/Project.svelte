0ec4f74b4093be4fb9cd2a851079ac55e307a614
April Hall <ari@arithefirst.com>
2025-02-12 15:02:55 -0500

    feat: Projects page

---
+++ src/components/Project.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  interface Props {
+    src: string;
+    alt: string;
+    title: string;
+    description: string;
+    badges?: string[];
+  }
+
+  const { src, alt, title, description, badges }: Props = $props();
+  const path = title.replaceAll(/[._+ ]/gm, '-').toLowerCase();
+</script>
+
+<a
+  class="bg-mantle flex h-48 w-full rounded-xl shadow-md transition-all duration-100 hover:scale-[1.01]"
+  href={`/projects/${path}`}
+>
+  <img class="bg-crust aspect-square w-48 rounded-l-xl object-cover" {src} {alt} />
+  <div class="flex-grow rounded-r-xl p-4">
+    <h1 class="text-2xl font-bold">{title}</h1>
+    <p class="text-subtext-0 font-roboto text-wrap">{description}</p>
+    {#if badges}
+      <div class="flex w-3/4 flex-wrap gap-2">
+        {#each badges as item}
+          <div class="bg-crust border-text font-roboto rounded-md border px-3">
+            {item}
+          </div>
+        {/each}
+      </div>
+    {/if}
+  </div>
+</a>
---
+++ src/layouts/Projects.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import Project from '$lib/components/Project.svelte';
+
+  interface ProjectType {
+    src: string;
+    alt: string;
+    title: string;
+    description: string;
+    badges: string[];
+  }
+
+  interface Props {
+    projects: ProjectType[];
+  }
+
+  const { projects }: Props = $props();
+</script>
+
+{#each projects as item}
+  <Project src={item.src} alt={item.alt} title={item.title} description={item.description} badges={item.badges} />
+{/each}
