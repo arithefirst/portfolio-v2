f5635ceeed2466d7bb1ff3099eb1b4195dd4139b
April Hall <ari@arithefirst.com>
2025-02-13 11:03:42 -0500

    feat: Propegate projects

--- src/components/Project.svelte
+++ src/components/Project.svelte
@@ -12,7 +12,7 @@
 </script>
 
 <a
-  class="bg-mantle flex h-48 w-full rounded-xl shadow-md transition-all duration-100 hover:scale-[1.01]"
+  class="bg-mantle flex h-48 w-full min-w-128 rounded-xl shadow-md transition-all duration-100 hover:scale-[1.01]"
   href="projects/{path}"
 >
   <img class="bg-crust aspect-square w-48 rounded-l-xl object-cover" {src} {alt} />
@@ -20,7 +20,7 @@
     <h1 class="text-2xl font-bold">{title}</h1>
     <p class="text-subtext-0 font-roboto text-wrap">{description}</p>
     {#if badges}
-      <div class="flex w-3/4 flex-wrap gap-2">
+      <div class="flex w-11/12 flex-wrap gap-2 pt-2">
         {#each badges as item}
           <div class="bg-crust border-text font-roboto rounded-md border px-3">
             {item}
