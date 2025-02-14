c5f499dde48e1533164e99df64281211f7e5f0ae
April Hall <ari@arithefirst.com>
2025-02-14 08:20:20 -0500

    feat: Make command prompt button open command prompt

---
+++ src/components/PromptTriggerButton.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  interface Props {
+    children: Snippet;
+    class: string;
+  }
+
+  const { children, class: className }: Props = $props();
+
+  function dispatchKeypress() {
+    const ctrlKEvent = new KeyboardEvent('keydown', {
+      key: 'k',
+      code: 'KeyK',
+      ctrlKey: true,
+      bubbles: true,
+      cancelable: true,
+    });
+
+    document.dispatchEvent(ctrlKEvent);
+  }
+</script>
+
+<button class="cursor-pointer {className}" onclick={dispatchKeypress}>{@render children()}</button>
