6b306f32102d28bb9e2bd2be7e88599a6274d652
April Hall <ari@arithefirst.com>
2025-02-16 17:26:24 -0500

    feat: Dialog animations

--- src/components/ui/dialog/dialog-content.svelte
+++ src/components/ui/dialog/dialog-content.svelte
@@ -4,6 +4,7 @@
   import type { Snippet } from 'svelte';
   import * as Dialog from './index.js';
   import { cn } from '$lib/utils.js';
+  import { blur } from 'svelte/transition';
 
   let {
     ref = $bindable(null),
@@ -18,8 +19,17 @@
 </script>
 
 <Dialog.Portal {...portalProps}>
-  <Dialog.Overlay class="bg-crust/80" />
+  <Dialog.Overlay forceMount class="bg-crust/80">
+    {#snippet child({ props, open })}
+      {#if open}
+        <div {...props} transition:blur={{ duration: 200 }}>
+          <!-- ... -->
+        </div>
+      {/if}
+    {/snippet}
+  </Dialog.Overlay>
   <DialogPrimitive.Content
+    forceMount
     bind:ref
     class={cn(
       'bg-background animate-blur  border-surface-1 fixed top-[50%] left-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border p-6 shadow-lg duration-200 sm:rounded-lg',
@@ -27,7 +37,13 @@
     )}
     {...restProps}
   >
-    {@render children?.()}
+    {#snippet child({ props, open })}
+      {#if open}
+        <div {...props} transition:blur={{ duration: 200 }}>
+          {@render children?.()}
+        </div>
+      {/if}
+    {/snippet}
     <DialogPrimitive.Close
       class="ring-offset-background focus:ring-ring data-[state=open]:bg-accent data-[state=open]:text-muted-foreground absolute top-4 right-4 rounded-sm opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-none disabled:pointer-events-none"
     >
