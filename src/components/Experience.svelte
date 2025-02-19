6418cf7ec7f939e5bfe7e6bd0c536d1bebb4d6f6
April Hall <ari@arithefirst.com>
2025-02-19 09:27:08 -0500

    feat: Experience page

---
+++ src/components/Experience.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  interface Props {
+    startDate: Date;
+    endDate: Date | 'Current';
+    company: string;
+    position: string;
+    bullets: string[];
+    src: string;
+    alt: string;
+  }
+
+  const { startDate, endDate, company, position, bullets, src, alt }: Props = $props();
+  const dateOpts: Intl.DateTimeFormatOptions = { year: 'numeric', month: 'short' };
+</script>
+
+<div class="bg-mantle flex items-center rounded-lg p-6 shadow-lg">
+  <img class="mr-6 size-24" {src} {alt} />
+  <div>
+    <p class="text-sm text-gray-400 italic">{position}</p>
+    <h3 class="text-xl font-bold">{company}</h3>
+    <p class="text-gray-400">
+      {startDate.toLocaleString('en-US', dateOpts)} - {endDate.toLocaleString('en-US', dateOpts)}
+    </p>
+    <ul class="mt-2 list-inside list-disc text-sm text-gray-300">
+      {#each bullets as bullet}
+        <li>{bullet}</li>
+      {/each}
+    </ul>
+  </div>
+</div>
---
+++ src/layouts/Experiences.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  interface ExperienceCard {
+    startDate: Date;
+    endDate: Date | 'Current';
+    company: string;
+    position: string;
+    bullets: string[];
+    src: string;
+    alt: string;
+  }
+
+  import Experience from '$lib/components/Experience.svelte';
+  const { cards }: { cards: ExperienceCard[] } = $props();
+</script>
+
+<div class="flex flex-col gap-3">
+  {#each cards as card}
+    <Experience
+      startDate={card.startDate}
+      endDate={card.endDate}
+      company={card.company}
+      position={card.position}
+      bullets={card.bullets}
+      src={card.src}
+      alt={card.alt}
+    />
+  {/each}
+</div>
