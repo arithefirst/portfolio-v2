8ae1c86a259d0fe96ba41280724750cf2c16fc2c
April Hall <ari@arithefirst.com>
2025-03-10 12:28:28 -0400

    feat: Tooltips

--- src/components/Link.svelte
+++ src/components/Link.svelte
@@ -1,14 +1,16 @@
 <script lang="ts">
   import type { Snippet } from 'svelte';
+  import * as Tooltip from '$lib/components/ui/tooltip/index.js';
 
   interface Props {
     href: string;
     email: boolean;
     class: string;
+    alt: string;
     children: Snippet;
   }
 
-  const { href, email, children, class: className }: Props = $props();
+  const { alt, href, email, children, class: className }: Props = $props();
 
   function openEmailLink(event: MouseEvent) {
     window.open(href, 'mail');
@@ -16,8 +18,17 @@
   }
 </script>
 
-{#if email}
-  <a {href} class={className} onclick={(event) => openEmailLink(event)}>{@render children()}</a>
-{:else}
-  <a {href} class={className} rel="noreferrer noopener" target="_blank">{@render children()}</a>
-{/if}
+<Tooltip.Provider>
+  <Tooltip.Root>
+    <Tooltip.Trigger class={className}>
+      {#if email}
+        <a {href} onclick={(event) => openEmailLink(event)}>{@render children()}</a>
+      {:else}
+        <a {href} rel="noreferrer noopener" target="_blank">{@render children()}</a>
+      {/if}
+    </Tooltip.Trigger>
+    <Tooltip.Content class="bg-crust text-text border-base border">
+      <p>{alt}</p>
+    </Tooltip.Content>
+  </Tooltip.Root>
+</Tooltip.Provider>
