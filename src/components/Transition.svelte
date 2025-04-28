1a76063fc1e024259200474e66a6551b2d1e2532
April Hall <ari@arithefirst.com>
2025-04-27 21:31:20 -0400

    feat: Animations for some pages

---
+++ src/components/Transition.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import type { Snippet } from 'svelte';
+  import { blur } from 'svelte/transition';
+  import { onMount } from 'svelte';
+
+  interface Props {
+    children: Snippet;
+    delay?: number;
+    duration?: number;
+    class: string;
+  }
+
+  const { children, delay = 0, duration = 400, class: className }: Props = $props();
+  let loaded: boolean = $state(false);
+  onMount(() => (loaded = true));
+</script>
+
+{#if loaded}
+  <div in:blur={{ delay, duration }} class={className}>
+    {@render children()}
+  </div>
+{/if}
